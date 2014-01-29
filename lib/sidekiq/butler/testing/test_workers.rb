69c0c916a5d884b85b61d40f76cbd32f5ff758fa
Jan van der Pas <janvanderpas@gmail.com>
2014-01-29 01:32:38 +0100

    Add testworkers

--- lib/sidekiq/butler/testing/test_workers.rb
+++ lib/sidekiq/butler/testing/test_workers.rb
@@ -1,7 +1,19 @@
 module Sidekiq
   module Butler
     module Testing
-      class TestWorker
+      class Worker
+        include Sidekiq::Worker
+
+        def perform
+        end
+      end
+      class WorkerWithReservation
+        include Sidekiq::Worker
+
+        sidekiq_options butler: { reservation: { name: 'mies', guests: 2 } }
+
+        def perform
+        end
       end
     end
   end
